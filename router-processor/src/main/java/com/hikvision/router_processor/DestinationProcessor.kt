b8156d54338e59dcd5fce261e8f5323f5e1e1f2b
zhangbao <15056006309@163.com>
2021-10-28 00:45:12 +0800

    [Add]1.完成注解处理器的开发 [Add]2.完成发布注解库和注解处理器库脚本

--- router-processor/src/main/java/com/hikvision/router_processor/DestinationProcessor.kt
+++ router-processor/src/main/java/com/hikvision/router_processor/DestinationProcessor.kt
@@ -2,14 +2,18 @@ package com.hikvision.router_processor
 
 import com.google.auto.service.AutoService
 import com.hikvision.router.annotations.Destination
+import java.io.Writer
+import java.lang.RuntimeException
+import java.lang.StringBuilder
 import java.util.*
 import javax.annotation.processing.AbstractProcessor
+import javax.annotation.processing.Processor
 import javax.annotation.processing.RoundEnvironment
 import javax.annotation.processing.SupportedSourceVersion
 import javax.lang.model.SourceVersion
 import javax.lang.model.element.TypeElement
 
-@AutoService(value = [DestinationProcessor::class])
+@AutoService(value = [Processor::class])
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
 class DestinationProcessor : AbstractProcessor(){
 
@@ -33,14 +37,47 @@ class DestinationProcessor : AbstractProcessor(){
 
             if(allDestinationEnvironment!=null&& allDestinationEnvironment.isEmpty()) return false
 
+            val className = "RouterMapping_${System.currentTimeMillis()}"
+
+            val builder = StringBuilder()
+            builder.append("package com.hikvision.gradlelifecycle.mappinng;\n\n")
+            builder.append("import java.util.HashMap;\n")
+            builder.append("import java.util.Map;\n\n")
+            builder.append("public class $className {\n")
+            builder.append("    public static Map<String,String> get() {\n")
+            builder.append("        HashMap<String,String> map = new HashMap<>();\n")
+
+
             allDestinationEnvironment?.onEach {element->
                 val typeElement = element as TypeElement
                 val destination = typeElement.getAnnotation(Destination::class.java)
                 destination?.apply {
                     val realPath = typeElement.qualifiedName
                     println("$TAG >>> url:$url destination:$destination realPath:$realPath")
+                    builder.append("        map.put(\"$url\",\"$realPath\");\n")
                 }
             }
+
+            builder.append("        return map;\n")
+            builder.append("    }\n").append("}\n")
+
+            val mappingFullClassName = "com.hikvision.gradlelifecycle.mappinng.$className"
+            println("$TAG >>> mappingFullClassName = $mappingFullClassName")
+
+            println("$TAG >>> class content =\n$builder")
+
+            var writer:Writer? = null
+            try {
+                val sourceFile = processingEnv.filer.createSourceFile(mappingFullClassName)
+                writer = sourceFile.openWriter()
+                writer.write(builder.toString())
+                writer.flush()
+            } catch (e: Exception) {
+                throw RuntimeException("Error while create file",e)
+            }finally {
+                writer?.close()
+            }
+
             println("$TAG >>> process finish...")
         }
 
